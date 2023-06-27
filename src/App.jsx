03d7ef443fdaf66cbb015d9e252340af011568e3
Zurab Kobakhidze <zukakobakhidze@gmail.com>
2023-06-27 16:15:50 +0400

    add pages and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-
+import { Route, Routes, Link, Navigate } from "react-router-dom";
+import HomePage from './pages/Home/HomePage';
 
 
 function App() {
-  
-
   return (
-    <>
-      
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    
+    </Routes>
+  );
 }
 
-export default App
+export default App;

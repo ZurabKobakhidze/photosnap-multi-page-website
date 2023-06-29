d3b270d2046cbf4dc8c526496d21c4085d1a89aa
Zurab Kobakhidze <zukakobakhidze@gmail.com>
2023-06-30 00:45:20 +0400

    add styled components

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,15 +1,25 @@
 import { HamburgeMenu, LogoImage } from "assets/index";
 import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 
-
+const Main = styled.div`
+   width: 100%;
+   height: 73px;
+   display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding-left: 24px;
+    padding-right: 24px;
+    box-sizing: border-box;
+`;
 
 function Header() {
   return (
-    <div>
+    <Main>
       <img src={LogoImage} alt="" />
       <img src={HamburgeMenu} alt="" />
       
-    </div>
+    </Main>
   );
 }
 

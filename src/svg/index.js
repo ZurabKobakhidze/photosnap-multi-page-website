29762bd17a4f9c1eb9dd86b6ee7fd5c14e94f7be
Zurab Kobakhidze <zukakobakhidze@gmail.com>
2023-07-13 15:29:10 +0400

    fi svgs

--- src/svg/index.js
+++ src/svg/index.js
@@ -1,7 +1,7 @@
 export { default as White } from "./White";
 export { default as Youtube } from "./Youtube";
 export { default as Facebook} from "./Facebook";
-export { default as Pinterest} from "./pinterest";
-export { default as Twitter} from "./twitter";
-export { default as ArrowWhite} from "./arrowWhite";
+export { default as Pinterest} from "./Pinterest";
+export { default as Twitter} from "./Twitter";
+export { default as ArrowWhite} from "./ArrowWhite";
 export { default as Instagram} from "./Instagram";
